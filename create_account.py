304528392a88936c53239013c24ab85853dcea30
KURO1 <412681778@qq.com>
2017-02-22 13:51:59 +0800

    test for create_acconut

--- create_account.py
+++ create_account.py
@@ -5,18 +5,23 @@
 from bigchaindb_driver.crypto import generate_keypair
 import rapidjson
 
-keypair = generate_keypair()
-username = input ('Pick a username:\n')
+def create_account(username):
+    keypair = generate_keypair()
 
-account = {
-    'verifying_key' : keypair.verifying_key ,
-    'signing_key' : keypair.signing_key ,
-    'username' : username
-}
+    account = {
+        'verifying_key' : keypair.verifying_key ,
+        'signing_key' : keypair.signing_key ,
+        'username' : username
+    }
 
-print (account)
 
-f = open('.account','w')
-json_account = rapidjson.dumps(account)
-f.write(json_account)
-f.close()
+    f = open('.account','w')
+    json_account = rapidjson.dumps(account)
+    f.write(json_account)
+    f.close()
+    return(account)
+
+if __name__=='__main__':
+    username = input ('Pick a username:\n')
+    print(create_account(username))
+    

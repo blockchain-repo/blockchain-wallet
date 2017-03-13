54da54176a4eb8f45fc29a725f9df530aa58e4e1
KURO1 <412681778@qq.com>
2017-03-13 11:33:40 +0800

    rm stale test

--- tests/test_create_account.py
+++
@@ -1,7 +0,0 @@
-import os,sys
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0,parentdir)
-
-from create_account import create_account
-
-print(create_account('cindy'))

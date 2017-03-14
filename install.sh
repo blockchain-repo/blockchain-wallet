7fffd68c1b6e2bb94f29ff5134f24b8fa96913a2
Kuro1 <412681778@qq.com>
2017-03-14 09:08:27 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pip3 install 'cryptoconditions~=0.5.0'  'python-rapidjson~=0.0.6' 'pysha3>0.3'
+sudo pip3 install 'cryptoconditions~=0.5.0'  'python-rapidjson~=0.0.6' 'pysha3~=1.0.2'
